84ee854705cd4d19010ca19bb67c6530d1a54d59
Remaii <remaii1819@gmail.com>
2024-05-23 15:06:29 +0200

    eligibility.service.js: complete service

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -1,4 +1,40 @@
 class EligibilityService {
+  defineEligibility(value, condition) {
+    if (typeof condition === 'object') {
+      for (const [key, keyValue] of Object.entries(condition)) {
+        switch (key) {
+          case 'gt':
+            if (!(value > keyValue)) return false;
+            break;
+          case 'lt':
+            if (!(value < keyValue)) return false;
+            break;
+          case 'gte':
+            if (!(value >= keyValue)) return false;
+            break;
+          case 'lte':
+            if (!(value <= keyValue)) return false;
+            break;
+          case 'in':
+            if (!keyValue.includes(value)) return false;
+            break;
+          case 'and':
+            if (!Object.entries(keyValue).every(([op, val]) => this.defineEligibility(value, { [op]: val })))
+              return false;
+            break;
+          case 'or':
+            if (!Object.entries(keyValue).some(([op, val]) => this.defineEligibility(value, { [op]: val })))
+              return false;
+            break;
+          default:
+            return false;
+        }
+      }
+      return true;
+    }
+    return value == condition; /* exclude type compare */
+  }
+
   /**
    * Compare cart data with criteria to compute eligibility.
    * If all criteria are fulfilled then the cart is eligible (return true).
@@ -8,8 +44,26 @@ class EligibilityService {
    * @return {boolean}
    */
   isEligible(cart, criteria) {
-    // TODO: compute cart eligibility here.
-    return false;
+    for (const [key, condition] of Object.entries(criteria)) {
+      const keys = key.split('.');
+      let cartValue = cart;
+
+      for (const k of keys) {
+        if (cartValue === undefined)
+          return false;
+
+        if (Array.isArray(cartValue)) {
+          if (!cartValue.some(item => this.defineEligibility(item[k], condition)))
+            return false;
+          cartValue = cartValue.map(cartVal => cartVal[k]);
+        } else {
+          cartValue = cartValue[k];
+        }
+      }
+      if (!Array.isArray(cartValue) && !this.defineEligibility(cartValue, condition))
+        return false;
+    }
+    return true;
   }
 }
 
